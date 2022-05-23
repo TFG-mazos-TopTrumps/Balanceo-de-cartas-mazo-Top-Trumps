cc1330d38146f81d59ebb1a0b89abdb4191c6fa3
Pablosky971 <pablosky971@gmail.com>
2022-05-23 19:49:46 +0200

    Subida inicial relativa al desarrollo web: proyecto backend (Spring Boot)

---
+++ Servicio/GeneradorMazosTopTrumps/src/main/java/init/GeneradorMazosTopTrumpsApplication.java
@@ -0,0 +1,13 @@
+package init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneradorMazosTopTrumpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneradorMazosTopTrumpsApplication.class, args);
+	}
+
+}
---
+++ Servicio/GeneradorMazosTopTrumps/src/main/java/model/User.java
@@ -0,0 +1,24 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="users")
+public class User {
+	
+	private String username;
+	private String password;
+	private String name;
+
+}
